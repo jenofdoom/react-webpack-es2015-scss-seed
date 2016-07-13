a337ee4317544fb0ec8c9dae055b385ef5111be6
jenofdoom <j_zajac@email.com>
2016-07-13 22:27:23 +1200

    WIP Unit tests with ava

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+const test = require('ava')
+const ReactTestUtils = require('react-addons-test-utils')
+
+test('foo', t => {
+  // var renderer = ReactTestUtils.createRenderer() // TODO figure this out
+  // see https://github.com/kentcdodds/react-ava-workshop/blob/master/INSTRUCTIONS.md
+  t.pass()
+})
