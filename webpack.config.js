c8b88caf00c934e96f2b295c1b938fb648d4f882
Jen Zajac <jen@catalyst.net.nz>
2016-07-16 13:19:54 +1200

    Simplify webpack config and add .editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const autoprefixer = require('autoprefixer')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const merge = require('webpack-merge')
 const path = require('path')
+const webpack = require('webpack')
+const validate = require('webpack-validator')
 
 const sassLoaders = [
   'css-loader',
@@ -8,8 +11,14 @@ const sassLoaders = [
   'sass-loader?includePaths[]=' + path.resolve(__dirname, './src')
 ]
 
-module.exports = {
-  context: path.join(__dirname, './src'),
+const PATHS = {
+  src: path.join(__dirname, './src'),
+  dist: path.join(__dirname, './dist')
+}
+
+// config that is shared between all types of build
+const common = {
+  context: PATHS.src,
 
   entry: {
     javascript: './index.js',
@@ -18,7 +27,7 @@ module.exports = {
 
   output: {
     filename: 'app.js'
-    // don't need a path because this config only used by devserver
+    // don't need a path for default config
   },
 
   module: {
@@ -52,6 +61,45 @@ module.exports = {
   // set up resolve so don't have to qualify paths with ./ within src
   resolve: {
     extensions: ['', '.js', '.jsx', '.scss'],
-    root: [path.join(__dirname, './src')]
+    root: [PATHS.src]
   }
 }
+
+// build config from common plus custom config according to event trigger
+var config
+
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common, {
+      devtool: 'cheap-module-source-map',
+
+      output: {
+        path: PATHS.dist
+      },
+
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env': {NODE_ENV: JSON.stringify('production')}
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          compress: {
+            warnings: false,
+            screw_ie8: true // don't try and support IE6->8
+          }
+        })
+      ]
+    })
+    break
+  default:
+    config = merge(common, {
+      devtool: 'cheap-module-eval-source-map',
+
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env': {NODE_ENV: JSON.stringify('development')}
+        }),
+      ]
+    })
+}
+
+module.exports = validate(config)
